24d8e2cbea96fd661b49deab3391b4702bc9d4aa
Arthurvs01 <galo13arthur10@outlook.com.br>
2026-02-19 15:34:25 -0300

    limpando repositorio

--- core/text_loader.py
+++
@@ -1,9 +0,0 @@
-import os
-class TextLoader:
-    BASE_PATH = "content/texts/"
-    @staticmethod
-    def load(filename: str) -> str:
-        path = os.path.join(TextLoader.BASE_PATH, filename)
-
-        with open(path, "r", encoding="utf-8") as f:
-            return f.read()
